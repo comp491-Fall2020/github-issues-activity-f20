258669a3f731878fd2ee53b4e8ce4f0bf0fb510f
SeonghoLee4 <leese@dickinson.edu>
2020-09-12 07:39:50 +0900

    Fixed issue#14: changed name of the mult method to multiply to enhance readability.

--- Calculator.java
+++ Calculator.java
@@ -45,7 +45,7 @@ public class Calculator {
 	 *            a double
 	 * @return x * y
 	 */
-	public double mult(double x, double y) {
+	public double multiply(double x, double y) {
 		return x * y;
 	}
 
