d3f3a3a575a8be30db9971d6164fcb3a5ebf4bbd
carrolsong <songc@dickinson.edu>
2020-10-01 02:14:26 +0800

    cirPer rename is done.

--- Calculator.java
+++ Calculator.java
@@ -119,7 +119,7 @@ public class Calculator {
 	 *            the radius
 	 * @return the perimeter of a circle with radius r.
 	 */
-	public double cirPer(double r) {
+	public double cirPerimeter(double r) {
 		return 2 * Math.PI * r;
 	}
 
